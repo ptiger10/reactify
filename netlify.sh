19f7ba438ded360fe4b09c3ff4830cc8b89e8a82
Dave Fort <dhfort@gmail.com>
2019-06-02 20:57:49 -0700

    upgrade reactify in make script

--- netlify.sh
+++ netlify.sh
@@ -5,9 +5,13 @@ for dir in example/*/web; do
     cd $dir
     mkdir -p netlify
     cp index.html netlify
+    cp favicon.ico netlify
     if [ -f ./style.css ]
         then cp style.css netlify
     fi
+    cd ..
+    pub upgrade reactify
+    cd web
     dart2js main.dart -o netlify/main.dart.js
     cd $original
 done
